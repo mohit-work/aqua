d548946a1a5011ba5c010cc3ccdb86910cb8e016
Abbas Khan <abbaskhan21204@gmail.com>
2024-03-16 16:39:14 +0530

    "16:38 abbas"

--- lib/login.dart
+++ lib/login.dart
@@ -17,69 +17,82 @@ class _LoginPageState extends State<LoginPage> {
     }
   }
 
-  @override
- @override
 @override
 Widget build(BuildContext context) {
   return Scaffold(
-    backgroundColor: Color.fromARGB(248, 45, 43, 43),
+    
+    backgroundColor: Color.fromARGB(255, 61, 199, 199),
     body: Padding(
-      padding: const EdgeInsets.only(top: 100, left: 20, right: 20,bottom: 100),
+      padding: const EdgeInsets.only(top:170.0,left:20,right:20,),
       child: Container(
         decoration: BoxDecoration(
+          boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.3),
+                      spreadRadius: 5,
+                      blurRadius: 5,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
           color: Colors.black,
-          borderRadius: BorderRadius.circular(10),
+          borderRadius: BorderRadius.circular(30),
+          border: Border.all(width: 1), // Add border to the container
         ),
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                SizedBox(height: 16),
-                Text(
-                  "LOGIN PAGE",
-                  style: TextStyle(color: Colors.white, fontSize: 30),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 16),
-                TextFormField(
-                  controller: _emailController,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(labelText: 'Email'),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Please enter your email';
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 16.0),
-                TextFormField(
-                  controller: _passwordController,
-                  obscureText: true,
-                  decoration: InputDecoration(labelText: 'Password'),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Please enter your password';
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 16.0),
-                ElevatedButton(
-                  onPressed: _login,
-                  child: Text('Login'),
-                ),
-                SizedBox(height: 16.0),
-                TextButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/signup');
-                  },
-                  child: Text('Don\'t have an account? Sign up'),
-                ),
-              ],
+        child: SizedBox(
+          height: 450, // Set a fixed height for the Container
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  SizedBox(height: 16),
+                  Text(
+                    "LOGIN PAGE",
+                    style: TextStyle(color: Colors.white, fontSize: 30),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 16),
+                  TextFormField(
+                    style:TextStyle(color: Color.fromARGB(255, 244, 249, 249)),
+                    controller: _emailController,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(labelText: 'Email'),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter your email';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 16.0),
+                  TextFormField(
+                    style:TextStyle(color: Color.fromARGB(255, 241, 247, 247)),
+                    controller: _passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(labelText: 'Password'),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter your password';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 16.0),
+                  ElevatedButton(
+                    onPressed: _login,
+                    child: Text('Login'),
+                  ),
+                  SizedBox(height: 16.0),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/signup');
+                    },
+                    child: Text('Don\'t have an account? Sign up'),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
